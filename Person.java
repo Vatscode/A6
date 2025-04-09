9d37aac94572b4d8f61f322abf03dcf3d9339d21
Vatscode <becomeharkirat1112@gmail.com>
2025-04-09 13:23:13 -0300

    Person.java ✅

--- Person.java
+++ Person.java
@@ -1,29 +1,42 @@
+
 /**
  * A class representing a person -- suitable for extending into interesting
  * kinds of people -- like students or truck-drivers.
  *
- * @author Mark Young (A00000000)
+ * @author Vats Upadhyay (A00454163)
  */
 public class Person {
 
     // ********** instance variables ********** //
     private String name;
+    private String sex;
+    private int age;
+    private double height;
+    private double weight;
 
     // ********** constructors ********** //
     /**
-     * Create a Person with the given name.
+     * Create a Person with the given name and other details.
      *
-     * @param initialName this Person's name
+     * @param name this Person's name
+     * @param sex this Person's sex (e.g., "M" or "F")
+     * @param age this Person's age
+     * @param height this Person's height
+     * @param weight this Person's weight
      */
-    public Person(String initialName) {
-        name = initialName;
+    public Person(String name, String sex, int age, double height, double weight) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
     }
 
     /**
-     * Create a person with a place-holder name.
+     * Create a person with a place-holder name and values.
      */
     public Person() {
-        this("Baby Human");
+        this("Baby Human", "M", 0, 0.0, 0.0);
     }
 
     // ********** instance methods ********** //
@@ -46,10 +59,47 @@ public class Person {
     }
 
     /**
+     * Return this Person's sex.
+     *
+     * @return this Person's sex.
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * Return this Person's age.
+     *
+     * @return this Person's age.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Return this Person's height.
+     *
+     * @return this Person's height.
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Return this Person's weight.
+     *
+     * @return this Person's weight.
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
      * Provide a simple report on this Person.
      */
     public void writeOutput() {
-        System.out.println("Name: " + name);
+        System.out.println("Name: " + name + ", Sex: " + sex + ", Age: " + age + ", Height: " + height + ", Weight: " + weight);
+
     }
 
     /**
@@ -59,7 +109,9 @@ public class Person {
      * @return true if these people's names are identical; false otherwise.
      */
     public boolean hasSameName(Person other) {
-        if (other == null) return false;
+        if (other == null) {
+            return false;
+        }
         return name.equalsIgnoreCase(other.name);
     }
 
@@ -70,8 +122,37 @@ public class Person {
      */
     @Override
     public String toString() {
-        return "Person: " + name;
+        return "Person: " + name + ", " + sex + ", " + age + ", " + height + ", " + weight;
+
     }
 
-}
+      // ********** Comparators for sorting ********** //
+    public static Comparator<Person> nameComparator = new Comparator<Person>() {
+        @Override
+        public int compare(Person p1, Person p2) {
+            return p1.name.compareToIgnoreCase(p2.name);
+        }
+    };
 
+     public static Comparator<Person> ageComparator = new Comparator<Person>() {
+        @Override
+        public int compare(Person p1, Person p2) {
+            return Integer.compare(p1.age, p2.age);
+        }
+    };
+
+    public static Comparator<Person> heightComparator = new Comparator<Person>() {
+        @Override
+        public int compare(Person p1, Person p2) {
+            return Double.compare(p1.height, p2.height);
+        }
+    };
+
+     public static Comparator<Person> nameReverseComparator = new Comparator<Person>() {
+        @Override
+        public int compare(Person p1, Person p2) {
+            return p2.name.compareToIgnoreCase(p1.name);
+        }
+    };
+
+}
